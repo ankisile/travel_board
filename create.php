36dd9bd8df39ff73415f6784b213a5aa32e0b0dd
ankisile <ankisile@gmail.com>
2020-08-14 21:50:08 +0900

    write.php first commit

---
+++ create.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <a href="create.php">create</a>
+    <form action="process_create.php" method="POST">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p><textarea name="description" rows="8" cols="80"></textarea></p>
+      <p><input type="submit"></p>
+  </body>
+</html>
