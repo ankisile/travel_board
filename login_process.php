08b3335ce9a3e4e6a8dc2b2e8926f28e19bb3efd
ankisile <ankisile@gmail.com>
2020-08-22 03:26:44 +0900

    login/logout 기능 추가

---
+++ login_process.php
@@ -0,0 +1,29 @@
+<?php
+ //로그인은 세션 이용
+  session_start();
+  $conn=mysqli_connect("localhost", "root", "111111", "board");
+
+  $id=$_POST['id'];
+  $pw=$_POST['pw'];
+
+  $sql = "select * from member where id='$id'";
+  $result = mysqli_query($conn,$id);
+  $member = mysqli_fetch_array($result);
+  if($member>0){//중복되는 것이 있으면 이것은 id가 있다는 것
+    $hash_pw=$member['pw'];
+    if(password_verify($pw,$hash_pw)){
+      $_SESSION['user_id']=$id;
+      $_SESSION['user_pw']=$pw;
+      echo "<script>alert('로그인되었습니다.');
+      location.replace('./index.php');</script>";
+    }
+    else{
+      echo "<script>alert('아이디 혹은 비밀번호를 확인하세요');
+      history.back();</script>";
+    }
+  }else{
+    echo "<script>alert('아이디 혹은 비밀번호를 확인하세요');
+    history.back();</script>";
+  }
+
+ ?>
